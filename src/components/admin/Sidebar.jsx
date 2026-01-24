d49d22719bc6fcad0a9a04b5eefea48f9caaf493
Sahilghoghari <sahilghoghari07@gmail.com>
2026-01-24 10:53:21 +0530

    Toggle Fullscreen Added

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
+import { BsBoxSeam } from 'react-icons/bs';
+import { FaUsers } from 'react-icons/fa';
+import { HiComputerDesktop } from 'react-icons/hi2';
 import { NavLink } from 'react-router-dom'
 
 function Sidebar() {
 
     const navClass = ({ isActive }) =>
-        `block my-2 mx-5 px-3 py-2 rounded-md transition-all
+        `flex items-center gap-x-3 my-2 mx-5 px-3 py-2 rounded-md transition-all
         ${isActive
             ? "bg-orange-500 text-black"
-            : "hover:bg-orange-500 hover:text-black"
+            : "hover:bg-orange-200 hover:text-black hover:translate-x-1.5"
         }`;
 
     return (
         <aside className='bg-black h-screen fixed top-0 left-0 text-white font-semibold transition-all duration-300 w-[18%] flex flex-col justify-between'>
 
             <nav className='mt-16'>
-                <NavLink to='/admin' end className={navClass}>Dashboard</NavLink>
-                <NavLink to='/admin/products' className={navClass}>Products</NavLink>
-                <NavLink to='/admin/users' className={navClass}>Users</NavLink>
+                <NavLink to='/admin' end className={navClass}><HiComputerDesktop />Dashboard</NavLink>
+                <NavLink to='/admin/products' className={navClass}><BsBoxSeam />Products</NavLink>
+                <NavLink to='/admin/users' className={navClass}><FaUsers />Users</NavLink>
             </nav>
             <div className='mb-14'>
                 <NavLink to='/login' className='block my-3 mx-5 px-3 py-2 rounded-md text-red-500 border-red-500 border hover:bg-red-500 hover:text-black transition-all'>Logout</NavLink>
--- src/components/admin/Topbar.jsx
+++ src/components/admin/Topbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { AiOutlineFullscreenExit } from 'react-icons/ai';
+import { CgRatio } from 'react-icons/cg';
 
 function Topbar() {
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleFullScreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  }
+
+  // Sync state if user exits fullscreen using ESC
+  useEffect(() => {
+    const handleChange = () => {
+      setIsFullscreen(!!document.fullscreenElement);
+    };
+
+    document.addEventListener("fullscreenchange", handleChange);
+    return () => document.removeEventListener("fullscreenchange", handleChange);
+  }, []);
+
   return (
-    <header className='fixed top-0 w-full z-1 bg-black text-center py-1'>
+    <header className='fixed top-0 w-full z-1 bg-black flex justify-between py-1 px-8'>
       <h1 className='text-3xl font-bold text-orange-500'>Admin Dashboard</h1>
+      <button
+        onClick={toggleFullScreen}
+        title={isFullscreen ? 'Exit Fullscreen' : 'Enter Fullscreen'}
+        className='cursor-pointer text-2xl rounded-full p-2 bg-orange-500 hover:bg-orange-400 hover:scale-95 transition-all'
+      >
+        {isFullscreen ? <AiOutlineFullscreenExit /> : <CgRatio />}
+      </button>
     </header>
   )
 }
