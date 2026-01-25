7f27d7d135324864b3a866ac82757eb2d603533d
Sahilghoghari <sahilghoghari07@gmail.com>
2026-01-25 21:45:12 +0530

    Add Products in admin panel with json server

---
+++ src/components/admin/Cards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Cards({ icon, iconBg, iconColor, title, value }) {
+    return (
+        <div className='bg-white rounded-xl shadow-md p-5 hover:shadow-2xl hover:-translate-y-1.5 transition-all flex items-center gap-3'>
+            <div className={`${iconBg} ${iconColor} rounded-full text-2xl p-3`}>
+                {icon}
+            </div>
+            <div className='font-semibold'>
+                <h2 className='text-lg'>{title}</h2>
+                <h3 className='text-gray-600 text-4xl'>{value}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
