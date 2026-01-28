a2e3fd1b5b54978a03db6d5a825698e9a09e2f7e
Sahilghoghari <work.sahilghoghari07@gmail.com>
2026-01-28 16:57:48 +0530

    Toast added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./containers/Login";
 import Register from "./containers/Register";
 import UserRoutes from "./routes/UserRoutes";
 import AdminRoutes from "./routes/AdminRoutes";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -14,6 +15,15 @@ function App() {
         <Route path="/admin/*" element={<AdminRoutes />} />
         <Route path="/*" element={<UserRoutes />} />
       </Routes>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        closeOnClick={true}
+        draggable
+        pauseOnHover
+        transition={Bounce}
+      />
     </>
   );
 }
